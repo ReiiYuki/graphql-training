0c7cf4e5afd9fa9998b333c4e2f90cfebf1c4ab9
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-02-27 21:05:19 +0700

    Apollo *0*

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// server.js
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var app = express();
 var PORT = process.env.port || 3000
 var MyGraphQLSchema = require('./graphql/schema');
+var cors = require('cors');
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: MyGraphQLSchema,
   graphiql: true
